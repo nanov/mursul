9279516501626e6b800d30c11634b893f4be5190
nanov <mitko.n@gmail.com>
2024-06-28 23:27:42 +0300

    wip(reactive-prompt): work on reactive prompt on POSIX

--- src/generator.c
+++ src/generator.c
@@ -1,11 +1,29 @@
 #include "mursul.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 #define QUIT_COMMAND ":q"
 #define CHEAT_COMMAND ":c"
 
 int main(void) {
-  FILE *f = fopen("src/commands.h", "w");
+  FILE *f = fopen("src/assets.h", "w");
+	FILE *fs = fopen("assets/dwight_idiot.mp3", "r");
+	fseek(fs, 0L, SEEK_END);
+	size_t sound_size = ftell(fs);
+	fseek(fs, 0L, SEEK_SET);
+	char* sound_buffer = malloc(sizeof(*sound_buffer) * sound_size);
+	fread(sound_buffer, 1, sound_size, fs);
+	fclose(fs);
+	fprintf(f, "int dwight_idiot = [");
+	for(size_t i = 0; i<sound_size;++i) {
+		fprintf(f, "%d", *sound_buffer++);
+		if (i!=sound_size-1)
+			fprintf(f,",");
+	}
+	fprintf(f, "];\n");
+
+
+	
   fprintf(f, "// Auto generated - do not edit\n");
   input_union quit_command = {.word_val = QUIT_COMMAND};
   fprintf(f, "#define QUIT_COMMAND_VALUE %d\n", quit_command.command_val);
--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
-#include <ctype.h>
 #include <curl/curl.h>
+#include <sys/ioctl.h>
 #include <raylib.h>
 #include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,8 +9,6 @@
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
-
-#include "mursul.h"
 
 #define MAX_GUESSES 5
 #define WORD_LENGTH 6 // 5 + \0
@@ -27,17 +24,21 @@
 #define ASCII_RESET "\e[0m"
 #define ASCII_GREEN "\x1b[0;32m"
 #define ASCII_YELLOW "\x1b[0;33m"
+#define INPUT_INPUT_MODE 0
+#define INPUT_COMMAND_MODE 2
 
-typedef enum { WON, LOST, IN_PROGRESS } game_state;
+typedef enum { WON, LOST, IN_PROGRESS } game_phase;
 
 typedef struct {
   char *word;
-  game_state state;
+  game_phase state;
   size_t number_of_guesses;
   char guesses[MAX_GUESSES][WORD_LENGTH];
+  struct termios original_terminos;
+  struct termios raw_terminos;
 } game;
 
-size_t data_from_nyt(char *buffer, size_t item_size, size_t number_of_items,
+size_t data_from_nyt(char* buffer, size_t item_size, size_t number_of_items,
                      void *word_opaque) {
   size_t total_size = item_size * number_of_items;
   char *word = (char *)word_opaque;
@@ -101,123 +102,117 @@ char *get_word_from_file(char *file_path, char *word) {
   return word;
 }
 
-const input_union q = {.word_val = ".q"};
-
-void str_insert(char* source, char addition, size_t pos) {
-
+void init_terminal_state(game *state) {
+  tcgetattr(0, &state->original_terminos);
+  state->raw_terminos =
+      state->original_terminos; /* make new settings same as old settings */
+  state->raw_terminos.c_lflag &= ~ICANON; /* disable buffered i/o */
+  state->raw_terminos.c_lflag &= ~ECHO;   /* set echo mode */
 }
 
-#define INPUT_COMMAND_MODE 2
+int get_input(game *state, char *fill, char history[5][6],
+              size_t history_length) {
 
-int get_input(char *fill, char history[5][6], size_t history_length) {
-  static size_t desierd_len = WORD_LENGTH - 1;
-
-  static struct termios original_terminos, raw_terminos;
-  tcgetattr(0, &original_terminos);
-	raw_terminos = original_terminos; /* make new settings same as old settings */
-  raw_terminos.c_lflag &= ~ICANON; /* disable buffered i/o */
-  raw_terminos.c_lflag &= ~ECHO;   /* set echo mode */
-  tcsetattr(0, TCSANOW, &raw_terminos);
-
+  tcsetattr(0, TCSANOW, &state->raw_terminos);
   char f;
-	bool dirty = 0; // 1 = normal, 2 = command
-	int mode = 0; // 0 = normal, 2 = command
-	size_t pos = 0;
-	size_t len = 0;
-	size_t history_pos = history_length;
-	*fill = '\0';
+  bool dirty = 0; // 1 = normal, 2 = command
+  int mode = 0;   // 0 = normal, 2 = command
+  size_t pos = 0;
+  size_t len = 0;
+  size_t history_pos = history_length;
+  *fill = '\0';
   while (read(STDIN_FILENO, &f, 1) && f != '\n') {
-		switch (f) {
-			case 127: { // backspace
-				if (pos==0)
-					continue;
-				char* pos_ptr = fill + pos;
-				memmove(pos_ptr - 1, pos_ptr, 6 - pos);
-				pos--;
-				len--;
-				if (len == 0) {
-					mode = 0; // reset mode
-					dirty = 0;
-				}
-			};
-			break;
-			case 8: goto arrow_movement; // ctrl+h
-			case 12: goto arrow_movement; // ctrl+l
-			case 27: { // arrow keys
-				read(STDIN_FILENO, &f, 1);
-				read(STDIN_FILENO, &f, 1);
-			// TODO: not entierly happy with this
-			arrow_movement:	
-				switch(f) {
-					case 8:
-					case 68:
-						if (pos>0)
-							pos--;
-						break;
-					case 12:
-					case 67:
-						if (pos<len)
-							pos++;
-						break;
-					case 65:
-						if (history_pos == 0)
-							continue;
-						history_pos--;
-						goto history_move;
-					case 66:
-						if (history_pos >= history_length)
-							continue;
-						history_pos++;
-						goto history_move;
-					history_move:
-						if (dirty == 1)
-							continue;
-						
-						if (history_pos == history_length) {
-							fill[0]= '\0';
-							pos = 0;
-							len = 0;
-							mode = 0;
-						} else {
-							strncpy(fill, history[history_pos], 6);
-							pos = len = strlen(fill);
-							mode = fill[0] == ':' ? INPUT_COMMAND_MODE : 0;
-						}
-				} break;
-				case ':':
-					if (len != 0)
-						continue;
-					mode = INPUT_COMMAND_MODE;
-				case 'a'...'z':
-				case 'A'...'Z':
-					if (len + mode >= 5)
-						continue;
-					char* pos_ptr = fill + pos;
-					memmove(pos_ptr + 1, pos_ptr, 6 - (pos + 1));
-					*pos_ptr = f;
-					pos++;
-					len++;
-					dirty=1;
-				break;
-			};
-		}
-		printf(ASCII_CLEAR_LINE "\r%s",fill);
-		printf("\033[%zuG", pos+1);
-		fflush(stdout);
+    switch (f) {
+    case 127: { // backspace
+      if (pos == 0)
+        continue;
+      char *pos_ptr = fill + pos;
+      memmove(pos_ptr - 1, pos_ptr, 6 - pos);
+      pos--;
+      len--;
+      if (len == 0) {
+        mode = 0; // reset mode
+        dirty = 0;
+      }
+    }; break;
+    case 8:
+      goto move_left; // ctrl+h
+    case 12:
+      goto move_right; // ctrl+l
+    case 27: {             // arrow keys
+      read(STDIN_FILENO, &f, 1);
+      read(STDIN_FILENO, &f, 1);
+      switch (f) {
+      case 68:
+			move_left:
+        if (pos > 0)
+          pos--;
+        break;
+      case 67:
+			move_right:
+        if (pos < len)
+          pos++;
+        break;
+      case 65:
+        if (history_pos == 0)
+          continue;
+        history_pos--;
+        goto history_move;
+      case 66:
+        if (history_pos >= history_length)
+          continue;
+        history_pos++;
+        goto history_move;
+      history_move:
+        if (dirty == 1)
+          continue;
+
+        if (history_pos == history_length) {
+          fill[0] = '\0';
+          pos = 0;
+          len = 0;
+          mode = 0;
+        } else {
+          strncpy(fill, history[history_pos], 6);
+          pos = len = strlen(fill);
+          mode = fill[0] == ':' ? INPUT_COMMAND_MODE : 0;
+        }
+      }
+      break;
+    case ':':
+      if (len != 0)
+        continue;
+      mode = INPUT_COMMAND_MODE;
+    case 'a' ... 'z':
+    case 'A' ... 'Z':
+      if (len + mode >= 5)
+        continue;
+      char *pos_ptr = fill + pos;
+      memmove(pos_ptr + 1, pos_ptr, 6 - (pos + 1));
+      *pos_ptr = f;
+      pos++;
+      len++;
+      dirty = 1;
+      break;
+    };
+    }
+    printf(ASCII_CLEAR_LINE "\r%s", fill);
+    printf("\033[%zuG", pos + 1);
+    fflush(stdout);
   }
-  tcsetattr(0, TCSANOW, &original_terminos);
-	printf("\n");
-	
-	if (mode == 0)
-		return 0;
+  tcsetattr(0, TCSANOW, &state->original_terminos);
+  printf("\n");
 
-	if (fill[1] == QUIT_COMMAND)
-		return -5;
+  if (mode == 0)
+    return len == 5 ? 0 : -3;
 
-	if (fill[1] == CHEAT_COMMAND)
-		return -6;
+  if (fill[1] == QUIT_COMMAND)
+    return -5;
 
-	return -7;
+  if (fill[1] == CHEAT_COMMAND)
+    return -6;
+
+  return -7;
 }
 
 int compare_words_alt(char *word, char *input) {
@@ -277,7 +272,55 @@ int parse_args(int argc, char **argv, char **assets_path) {
   return result;
 }
 
+static game game_state = {
+      .state = IN_PROGRESS,
+      .number_of_guesses = 0,
+  };
+
+typedef struct {
+	unsigned short width;
+	size_t prompt_pos;
+	char prompt[1024];
+} terminal_info;
+
+static terminal_info term_info = { .prompt_pos = 0 };
+#define SPIT_PRINT(C) sprintf(&term_info.prompt[term_info.prompt_pos], C); \
+											 term_info.prompt_pos += strlen(C)
+#define SPIT_PRINTF(C, ...) sprintf(&term_info.prompt[term_info.prompt_pos], C, __VA_ARGS__); \
+											 term_info.prompt_pos += strlen(C)
+#define SPIT_OUTPUT() printf("%s", term_info.prompt); \
+											printf("\33[%dG (%zu/%d)\n", term_info.width-6, game_state.number_of_guesses, MAX_GUESSES); \
+											fflush(stdout)
+
+
+void fill_terminal_info(int sig) {
+	(void) sig;
+
+	struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+	unsigned short p_w = term_info.width;	
+	term_info.width = w.ws_col;
+	if (p_w > term_info.width) {
+		printf(ASCII_CLEAR_LINE ASCII_PREVIOUS_LINE ASCII_CLEAR_LINE ASCII_PREVIOUS_LINE ASCII_CLEAR_LINE);
+	} else {
+		printf(ASCII_CLEAR_LINE ASCII_PREVIOUS_LINE ASCII_CLEAR_LINE);
+	}
+	SPIT_OUTPUT();
+
+	// TODO: handle screen shrink
+}
+
+static char prompt[1028];
+
+
+
 int main(int argc, char **argv) {
+	fill_terminal_info(0);
+	signal(SIGWINCH, fill_terminal_info);
+  init_terminal_state(&game_state);
+	// TODO: move into one function
+  tcsetattr(0, TCSANOW, &game_state.raw_terminos);
+
   char word_to_match[WORD_LENGTH];
 
   char *assets_path = "assets/";
@@ -289,8 +332,7 @@ int main(int argc, char **argv) {
   if (c == -1) {
     return 0;
   } else if (c == 1) {
-    snprintf(asset_full_path, sizeof(asset_full_path), "%s/%s", assets_path,
-             "words.txt");
+    snprintf(asset_full_path, sizeof(asset_full_path), "%s/%s", assets_path, "words.txt");
     get_word_from_file(asset_full_path, word_to_match);
   } else {
     download_word_from_nyt(word_to_match);
@@ -305,28 +347,32 @@ int main(int argc, char **argv) {
   SetTraceLogLevel(LOG_ERROR);
   InitAudioDevice();
   snprintf(asset_full_path, sizeof(asset_full_path), "%s/%s", assets_path,
-           "stupid.mp3");
+           "dwight_idiot.mp3");
   Sound sound = LoadSound(asset_full_path);
 
-  game game_state = {
-      .state = IN_PROGRESS,
-      .number_of_guesses = 0,
-  };
+
+
 
   // game loop
   while (game_state.state == IN_PROGRESS) {
     if (game_state.number_of_guesses == MAX_GUESSES)
       break;
-    printf("please enter a 5 letter word:\n");
+
+		SPIT_PRINT("please enter a 5 letter word:");
+		// SPIT_PRINTF("\33[%dG (%zu/%d)\n", term_info.width-6, game_state.number_of_guesses, MAX_GUESSES);
+		SPIT_OUTPUT();
+
     char *current_word = game_state.guesses[game_state.number_of_guesses];
 
-    int input_result = get_input(current_word, game_state.guesses, game_state.number_of_guesses);
+    int input_result = get_input(&game_state, current_word, game_state.guesses,
+                                 game_state.number_of_guesses);
+		term_info.prompt_pos = 0;
     switch (input_result) {
     case -7:
-      printf("Unknown command, idiot! (%d)\n", game_state.number_of_guesses);
+      SPIT_PRINTF("Unknown command, idiot! (%zu)\n", game_state.number_of_guesses);
       continue;
     case -6:
-      printf("Not only and idiot, a cheater as well, shhhh, this is the "
+      SPIT_PRINTF("Not only and idiot, a cheater as well, shhhh, this is the "
              "word " ASCII_ITALIC ASCII_BOLD "%s" ASCII_RESET "\n",
              word_to_match);
       continue;
